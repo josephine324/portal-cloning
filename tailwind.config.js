b95be8318a546a48e80f594441d737d624d5b084
josephine324 <j.mutesi@alustudent.com>
2025-01-23 23:54:13 +0200

    cloning

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,13 +1,24 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["**/*.html"],
+  content: ["./*.html"],
   theme: {
     extend: {
       colors: {
         customGray:"#D5D5D5",
         customBlack: "#333333E6",
         customGreen: "#a18a4d",
+        customGrey: "#F2F2F2",
       },
+      spacing: {
+        '72': '18rem', // 288px
+        '80': '20rem', // 320px
+        '96': '24rem', // 384px
+        '112': '28rem', // 448px
+        '128': '32rem', // 512px
+      },
+      fontFamily: {
+        lato: ['Lato', 'sans-serif'],
+      }
     },
   },
   plugins: [],
