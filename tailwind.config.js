9131101598f2169615ec47aebb87a76238bf2b84
josephine324 <j.mutesi@alustudent.com>
2025-01-28 18:09:01 +0200

    finished project

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,24 +4,24 @@ module.exports = {
   theme: {
     extend: {
       colors: {
-        customGray:"#D5D5D5",
+        customGray: "#D5D5D5",
         customBlack: "#333333E6",
         customGreen: "#a18a4d",
         customGrey: "#F2F2F2",
-        customColor:"#A18A4D",
+        customColor: "#A18A4D",
+        customblack: "#333333",
       },
       spacing: {
-        '72': '18rem', // 288px
-        '80': '20rem', // 320px
-        '96': '24rem', // 384px
-        '112': '28rem', // 448px
-        '128': '32rem', // 512px
+        72: "18rem", // 288px
+        80: "20rem", // 320px
+        96: "24rem", // 384px
+        112: "28rem", // 448px
+        128: "32rem", // 512px
       },
       fontFamily: {
-        lato: ['Lato', 'sans-serif'],
-      }
+        lato: ["Lato", "sans-serif"],
+      },
     },
   },
   plugins: [],
-}
-
+};
